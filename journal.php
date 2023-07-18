6a6e5e28210516058e1dc7a3e3919a07b8344b4b
Hazeltron <paulhazelton.work@gmail.com>
2023-07-18 12:15:50 -0600

    updated journal

--- journal.php
+++ journal.php
@@ -2,6 +2,5 @@
 ?>
 
 <h1>Journal</h1>
-<h2>Date: Jul 17 2023</h2>
-<p>I'm feeling really run down. Like everyhting inn my life isn't working out. The girl I like doesn't feel the same, I'm stressed all the time. And I wonder, is it all worth it? Do you get to the end and think, I'm glad I was here? Because if you don't than why not end it now?</p>
-<p>I can't believe the only thing I have to talk to is this computer. But at least it wont tell anyone my secrets.</p>
+<h2>Date: Jul 18 2023</h2>
+<p>I'm feeling a little behind and like I haven't done enough in this course. Is that true? Am I being hard on myself?</p>
