21ef7fc4dda6f7f6479c3620386c069a3bb48445
Hazeltron <paulhazelton.work@gmail.com>
2023-07-18 13:01:18 -0600

    hello

--- journal.php
+++ journal.php
@@ -3,4 +3,4 @@
 
 <h1>Journal</h1>
 <h2>Date: Jul 18 2023</h2>
-<p>I'm feeling a little behind and like I haven't done enough in this course. Is that true? Am I being hard on myself?</p>
+<p>hello?</p>
