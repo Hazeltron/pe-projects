811cb8d8ba0e2a8ca5c4b08a3c459728fd05bdf3
Hazeltron <paulhazelton.work@gmail.com>
2023-08-01 23:17:31 -0600

    Added meta to head

--- header.php
+++ header.php
@@ -2,7 +2,15 @@
 <html>
 	<head>
 
+		<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="It's been a weird day.">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="Paul Hazelton">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Paul Hazelton</title>
+		
 
 		<link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Esteban|Oswald:300" >
 
