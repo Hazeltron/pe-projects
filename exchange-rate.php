4038f300e022158239cce1171306466cfc59f96f
Hazeltron <paulhazelton.work@gmail.com>
2023-08-01 23:15:44 -0600

    Added functionality to form

--- exchange-rate.php
+++ exchange-rate.php
@@ -30,11 +30,31 @@
 
 
 <?php
+	
+	
 
-	$usDollars = 0;
-	$euros = 0;
-	$rateFrom = 0;
-	$rateTo = 0;
+//part 1 -- init variables
+	$rate = 0.91;
+	$dollars = 10;
+	
+	
+
+//part 2 -- check user input
+	if( isset($_POST['submit']) ){
+		$rate = $_POST['euro-rate'];
+		$dollars = $_POST['dollars'];
+	}
+
+
+	
+
+//part 3 -- run calc
+	$euros = $dollars * $rate;
+
+	$formatedEuros = round($euros, 2);
+
+
+
  ?>
 
 
@@ -46,16 +66,19 @@
 	 	<p>This will tell you what euros mean in terms of dollars. That is to say, it'll show you the conversion of a given euro amount rounded up to the next penny--in dollars.</p>
 
 	 	<div class="field">
+	 		<pre><?php print_r($_POST) ?></pre>
 	 		
-	 		<label>How many Euros do you have?</label>
-	 		<input type="number" name="euros" min="0">
+	 		<label>How many dollars do you have?</label>
+	 		<input type="number" name="dollars" min="0" value="<?=$dollars?>">
 
 	 		<label>What is the current exchange rate?</label>
-	 		<input type="number" name="euro-rate" min="0">
+	 		<input type="number" name="euro-rate" step=".00000001" value="<?=$rate ?>">
 
 	 		<button name="submit" type="submit">Calculate</button>
 
 	 	</div>
+
+	 	<?php echo $formatedEuros; ?>
 
 	 </form>
 </div>
