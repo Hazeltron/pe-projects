0dfa9ba0cf852ab14f529e9c8c90679e4585fbea
Hazeltron <paulhazelton.work@gmail.com>
2023-08-06 00:45:12 -0600

    Added Theme challenge and made progress on header and call to action

---
+++ layout-theme-challenge/modules/call-to-action/template.php
@@ -0,0 +1,8 @@
+
+<call-to-action>
+	<h2 class='heading attention-voice'>Take a break.</h2>
+
+	<p class='story'>Come to sandals and escape the grind. Our exclusive island hosts multiple villas overseeing stunning sea side views. We offer inclusive trips and sand so soft your feet will melt through the grains. </p>
+
+	<a class='button' href='#'>Call to Action</a>
+</call-to-action>
---
+++ layout-theme-challenge/modules/graphic-diptych/template.php
@@ -0,0 +1,10 @@
+
+<graphic-diptych>
+	
+	<text-content>
+		
+		<h1 class='loud-voice'>Sandals</h1>
+
+	</text-content>
+
+</graphic-diptych>
