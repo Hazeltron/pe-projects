fea379478147f49015297908c9f46c9ed7ba5d73
Hazeltron <paulhazelton.work@gmail.com>
2023-08-06 22:06:02 -0600

    Updated colors and font sizes. Added copy.

--- layout-theme-challenge/data/articles.php
+++ layout-theme-challenge/data/articles.php
@@ -2,32 +2,32 @@
 
 $articles = [
 	[
-		"heading" => "Article or product",
-		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"heading" => "Scuba Diving",
+		"description" => "Come check out the coral reefs and fish and things.",
 		"thumbnail" => "scuba.png",
 	],
 	[
-		"heading" => "Destination or option",
+		"heading" => "Surfing",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
 		"thumbnail" => "surf.png",
 	],
 	[
-		"heading" => "Story or something",
+		"heading" => "Fishing",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
 		"thumbnail" => "fishing.png",
 	],
 	[
-		"heading" => "Banana tandori",
+		"heading" => "Get a Massage",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
 		"thumbnail" => "massage.png",
 	],
 	[
-		"heading" => "Bar B Q",
+		"heading" => "Fishing",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
 		"thumbnail" => "fishing.png",
 	],
 	[
-		"heading" => "Marsala",
+		"heading" => "Scuba Diving",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
 		"thumbnail" => "scuba.png",
 	],
