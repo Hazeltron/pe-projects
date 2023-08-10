758e264a0f9d4a4cd3d6a1d91e308da0fdfc5297
Hazeltron <paulhazelton.work@gmail.com>
2023-08-09 22:16:53 -0600

    Moved and added folders within new resume folder

---
+++ modules/resume/educ-contact-list/template.php
@@ -0,0 +1,31 @@
+<ul class="educ-contact-container">
+
+    <?php 
+        include("../../data-php/ed-contact-data.php");
+        foreach($facts as $fact){?>
+
+
+    <li class="education">
+
+        <h2>Education</h2>
+
+        <p>Colorado State University<br>
+        Fort Collins, CO<br>
+        Bachelor of Arts (B.A.) Mass Communication and Journalism (Jun 2016)</p>
+
+    </li>
+
+
+    <li class="contact">
+
+        <h2>Contact</h2>
+
+        
+        <p>paulhazelton.work@gmail.com</p>
+        <p>(719)337-0666</p>
+        <p>Colorado Springs, Co. 80903</p>
+    </li>
+
+    <?php } ?>
+          
+</ul>
