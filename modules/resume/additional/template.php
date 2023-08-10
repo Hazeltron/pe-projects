f6de1c8a6fd5b787626a323e421b93e9a1e4b868
Hazeltron <paulhazelton.work@gmail.com>
2023-08-10 00:21:41 -0600

    Added a loop to resume.php

---
+++ modules/resume/additional/template.php
@@ -0,0 +1,11 @@
+<section class="add">
+
+    <h2>Additional</h2>
+
+    <p>Programming<br>(HTML/CSS/PHP/JavaScript)</p>
+
+    <h2>Certifications</h2>
+
+    <p>TESOL Cert. IV (10429NAT)</p>
+
+</section>
