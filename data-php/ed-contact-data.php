8176a58d27b68cee9dc53ba7ae0053832a9e48c3
Hazeltron <paulhazelton.work@gmail.com>
2023-08-09 22:13:24 -0600

    Added blog cards data and data for resume

---
+++ data-php/ed-contact-data.php
@@ -0,0 +1,34 @@
+<?php  
+
+$ =[
+	[
+		"image" => "../../assets/blog.png",
+		"title" => "Beginning of class",
+		"description" => "This is a blog post.",
+	],
+		[
+		"image" => "../../assets/blog.png",
+		"title" => "Beginning of class",
+		"description" => "This is a blog post.",
+	],
+		[
+		"image" => "../../assets/blog.png",
+		"title" => "Beginning of class",
+		"description" => "This is a blog post.",
+	],
+		[
+		"image" => "../../assets/blog.png",
+		"title" => "Beginning of class",
+		"description" => "This is a blog post.",
+	],
+		[
+		"image" => "../../assets/blog.png",
+		"title" => "Beginning of class",
+		"description" => "This is a blog post.",
+	],
+];
+
+
+
+
+?>
