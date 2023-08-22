229c3486a1d9ed3772fe2448d88320e7ec5aca39
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 12:57:40 -0600

    Moved projects nav to bottom

---
+++ projects.php
@@ -0,0 +1,60 @@
+
+    	
+<?php 
+	include "header.php";
+	include "project-data.php";
+
+	$page = null;
+
+	if ( isset($_GET['page']) ){
+		$page = $_GET['page'];
+	} else {
+		$page = 'home';
+	}
+	
+
+
+?>
+
+<?php if ($page == 'home') { 
+	include "summary.php";
+} ?>
+
+ <?php if ($page == 'projects list') { 
+ 	include "projects-list.php";
+} ?>
+
+ <?php if($page == 'project details'){
+ 	include "project-details.php";
+} ?>
+<?php if($page == 'create'){
+ 	include "create.php";
+} ?>
+
+<section>
+	<div class="inner-column">
+		<nav>
+			<ul>
+
+				<li>
+					<a href="?page=home" class="<?php if ($page == "home"){ echo 'active';} ?>">Summary</a>
+				</li>
+
+				<li>
+					<a href="?page=projects list" class="<?php if ($page == 'projects list'){ echo "active";} ?>">Project List</a>
+				</li>
+
+				<li>
+					<a href="?page=create" class="<?php if ($page == "create"){ echo 'active';} ?>">Add</a>
+				</li>
+
+			</ul>
+		</nav>
+	</div>
+</section>
+
+
+<?php 
+	include "footer.php";
+?>
+
