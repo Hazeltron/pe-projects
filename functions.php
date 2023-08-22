28f5a4bc3530d420a3badc8e4f507c4015928da9
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 13:43:43 -0600

    Commented out error function

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php 
-function showErrors() {
-	ini_set('display_errors', 1);
-	ini_set('displayy_startup_errors', 1);
-	error_reporting(E_ALL);
-}
+// function showErrors() {
+// 	ini_set('display_errors', 1);
+// 	ini_set('displayy_startup_errors', 1);
+// 	error_reporting(E_ALL);
+// }
 	
-showErrors(); 
+// showErrors(); 
