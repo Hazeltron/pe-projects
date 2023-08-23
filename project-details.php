42302c89d8c764bbd38cec481c8df2c7f1cc2832
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 18:05:38 -0600

    Added font styles

--- project-details.php
+++ project-details.php
@@ -21,16 +21,16 @@ foreach($projectData as $data){
 <?php if(isset($page)){?>
 
 
-<section>
+<section class="project-detail">
 	<div class="inner-column">
 		
-		<h1><?=$page["name"]?></h1>
+		<h1 class="loud-voice"><?=$page["name"]?></h1>
 
-			<picture>
-				<img src="https://m.media-amazon.com/images/I/71V0QQRO9fL.jpg" alt="">
-			</picture>
+		<picture>
+			<img src="https://m.media-amazon.com/images/I/71V0QQRO9fL.jpg" alt="">
+		</picture>
 
-		<h2>Case Study hi</h2>
+		<h2 class="attention-voice">Case Study</h2>
 
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, vero vitae. In esse debitis distinctio accusantium quisquam, odit molestias, sequi?</p>
 
