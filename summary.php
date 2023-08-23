ce6fc25930ea3b72ebbd9abb83d9ee0777655aee
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 18:04:41 -0600

    Deleted annoying hero

--- summary.php
+++ summary.php
@@ -1,22 +1,8 @@
-<section>
+
+
+<section class="projects-summary">
     <div class="inner-column">
-        <div class="landing-image">
-
-            <section class="trans-message">
-
-
-                <h1>Projects</h1>
-                <p>View what I've made.</p>
-
-
-            </section>
-     
-    </div>
-</section>
-
-<section>
-    <div class="inner-column">
-		 	<h2>Projects Summary</h2>
+		 	<h2 class="loud-voice">Projects Summary</h2>
 		 	<p>These are projects that I've made while learning to program and design. Infact this page is it's own project. It uses querry strings using PHP to esentially create a website within a website.</p>
     </div>
 </section>
