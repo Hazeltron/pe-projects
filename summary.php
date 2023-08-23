28886a44e4f6a15b2bb6cfb8477ade542d8d0260
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 22:19:20 -0600

    Deleted confusing details pages

--- summary.php
+++
@@ -1,8 +0,0 @@
-
-
-<section class="projects-summary">
-    <div class="inner-column">
-		 	<h2 class="loud-voice">Projects Summary</h2>
-		 	<p>These are projects that I've made while learning to program and design. Infact this page is it's own project. It uses querry strings using PHP to esentially create a website within a website.</p>
-    </div>
-</section>
