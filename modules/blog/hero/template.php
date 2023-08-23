9dc182414da31d1df90f5a084fd18878d3c23804
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 22:21:52 -0600

    Deleted many of the modules/edited name for xp-data

--- modules/blog/hero/template.php
+++
@@ -1,8 +0,0 @@
-<div class="landing-image">
-        
-            <section class="trans-message">
-                <h1 class="loud-voice">Blog</h1>
-                <p>Feel free to explore my blog and consider subscribing while you're there.</p>
-            </section>
-
-        </div>
--- modules/home/hero/template.php
+++
@@ -1,7 +0,0 @@
-<!-- <div class="landing-image">
-		
-				<section class="trans-message">
-					<h1 class="loud-voice">Paul Hazelton</h1>
-					<p>I'm a programer and designer studying to create visually stunning, highly functional websites that are accessible to all.</p>
-				</section>
-		</div> -->
