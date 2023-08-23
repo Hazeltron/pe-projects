fdce07f670480f76598bf859e466a30f131d361b
Hazeltron <paulhazelton.work@gmail.com>
2023-08-22 18:05:27 -0600

    Added font styles

--- projects-list.php
+++ projects-list.php
@@ -8,23 +8,23 @@ include "project-data.php";
 
 ?>
 
-<section>
-
+<section class="project-list">
 <div class="inner-column">
 
 
 <?php foreach ($projectData as $data) { ?>
 
-	<h1 class="name"><?=$data["name"]?></h1>
+	<h1 class="name attention-voice"><?=$data["name"]?></h1>
+	
 	<picture>
 		<img src="#" alt="#">
 	</picture>
 
-	<h3 class="updated">Last updated: <?=$data["updated"]?></h3>
-	<h3 class="languages">Languages used: <?=$data["languages"]?></h3>
-	<h3 class="funtion">Purpose: <?=$data["function"]?></h3>
+	<h3 class="updated strong-voice">Last updated: <?=$data["updated"]?></h3>
+	<h3 class="languages strong-voice">Languages used: <?=$data["languages"]?></h3>
+	<h3 class="funtion strong-voice">Purpose: <?=$data["function"]?></h3>
 
-	<a href='?page=project details&data=<?=$data["id"]?>'>Learn more!</a>
+	<a class="project-link" href='?page=project details&data=<?=$data["id"]?>'>Learn more</a>
 
 
 <?php } ?>
