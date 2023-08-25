0b88ab15f551f41aee60ea5aa834bc340f2fd2f4
Hazeltron <paulhazelton.work@gmail.com>
2023-08-24 19:18:46 -0600

    Added more data into loop for projects page

--- pages/projects/index.php
+++ pages/projects/index.php
@@ -1,5 +1,14 @@
 <?php include('../../header.php') ?>
 
+<?php 
+
+$json = file_get_contents('../../data-php/project-page.json');
+$projectSections = json_decode($json, true);
+
+?>
+
+
+
 <section>
 	<div class="inner-column">
 
@@ -15,144 +24,34 @@
 	<div class="inner-column">
 
 		<h2 class="attention-voice">Websites</h2>
-		<p>These are websites I've created as practice. In decending order, it goes oldest to most recent.</p>
+		<p>These are websites I've created as practice. In descending order, it goes oldest to most recent.</p>
 
 	</div>
 </section>
 
+
+
+<?php foreach($projectSections as $section){ ?>
 <section>
 	<div class="inner-column">
 
-		<h3 class="strong-voice">Cult Site</h3>
+		<h3 class="strong-voice"><?=$section['heading']?></h3>
+
 		<picture>
-			<img src="#" alt="#">
+			<img src="<?=$section['image']?>" alt="#">
 		</picture>
-		<p>This is the first website I ever made.</p>
-		<p>I was teaching myself in Asia over the pandemic, spending hours a day without any real idea as to what I was doing. With that in mind I think it was fairly sucessful. I plan to come back to this someday.</p>
-		<a href="mythos-project">View the project</a>
-		
-	</div>
-</section>
+
+<?php foreach($section['description'] as $para){ ?> 
+		<p><?=$para?></p>
 
 		
-<section class="cakes">
-	<div class="inner-column">
+<?php } ?> 
 
-		<h3 class="strong-voice">Cakes by Gregory</h3>
-		<picture>
-			<img src="#" alt="#">
-		</picture>
-		<p>This project was foor an imaginary client named Gregory. He's a bit weird.</p>
-		<p>He only makes white cakes and wanted to break into the New York soocialite scene. I did my best to deliver a simple site that might attract his clientel.</p>
-		<a href="cake">View the project</a>
-		
-	</div>
-</section>
-
-		
-
-<section class="layout-challenge">
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Layout Challenge</h3>
-		<picture>
-			<img src="#" alt="#">
-		</picture>
-		<p>This project was more about how to make a responsive layout.</p>
-		<p>It's content is... questionable at best but that wasn't the goal!</p>
-		<a href="layout-challenge">View the project</a>
-		
-	</div>
-</section>
-
-		
-<section class="super-responsive">
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Super Responsive Layout</h3>
-		<picture>
-			<img src="#" alt="#">
-		</picture>
-		<p>This was another, much more serious responsive layout project.</p>
-		<p>I was handed a bit of html and a bit of CSS and told to make it look like an image we were provided. It might not look like much but it was a lot for me back then.</p>
-		<a href="super-responsive-layout">View the project</a>
-		
-	</div>
-</section>
-
-
-<section>
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Monster Adoption Agency</h3>
-		<picture>
-			<img src="" alt="">
-		</picture>
-		<p>This was a project with PHP that was my introduction to using data to generate content.</p>
-		<a href="php-practice/monsters.php">View the project</a>
-		
-	</div>
-</section>
-		
-
-<section>
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Sandals</h3>
-		<picture>
-			<img src="#" alt="#">
-		</picture>
-		<p>This is an attempt to emulate a luxory resort on a beach... Like Sandals.</p>
-		<p>Full disclosure: I was given a layout and wasn't allowed to change any of the HTML. I was meant to only edit this with CSS. It was a "Theme Challenge."</p>
-		<a href="layout-theme-challenge">View the project</a>
-		
-	</div>
-</section>
-
-
-<section>
-	<div class="inner-column">
-
-		<h2 class="attention-voice">Forms</h2>
-		<p>Everyone needs a form, right? Logins, user input, all that is super important. Well, I can do it and here's proof.</p>
+		<a href="<?=$section['link']?>">View the project</a>
 
 	</div>
 </section>
 
-
-<section>
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Dollars to Euros Converter</h3>
-		<picture>
-			<img src="" alt="">
-		</picture>
-		<p>This form is meant to take dollars and convert them to Euros.</p>
-		<p>It's helpful if you're traveling. Try it out.</p>
-		<a href="php-forms/exchange-rate.php">View the project</a>
-		
-	</div>
-</section>
-
-<section>
-	<div class="inner-column">
-
-		<h3 class="strong-voice">Drinks to Guest Calculator</h3>
-		<picture>
-			<img src="" alt="">
-		</picture>
-		<p>I had this idea for a calculator that would take an input of guests and how many drinks a bar or hotel had on hand. Then it would quickly do some math and teell you how many drinks each of those guests could have.</p>
-		<p>Not much of a use case here, but it was more about practice.</p>
-		<a href="php-forms/drink-form.php">View the project</a>
-		
-	</div>
-</section>
-		
-
-
-
-
-
-
+<?php } ?> 
 
 <?php include('../../footer.php') ?>
