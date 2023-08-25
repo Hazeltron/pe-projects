51808f40f4c8f7834d0794e61f8004a25ba03103
Hazeltron <paulhazelton.work@gmail.com>
2023-08-24 19:20:34 -0600

    Added layout garden data

---
+++ data-php/layout-card-data.php
@@ -0,0 +1,36 @@
+
+
+<?php
+
+$articles = [
+	[
+		"heading" => "Scuba Diving",
+		"description" => "Come check out the coral reefs and fish and things. Lorem ipsum dolor sit amet.",
+		"thumbnail" => "../assets/scuba.png",
+	],
+	[
+		"heading" => "Surfing",
+		"description" => "Come surf with us. It would be a lot of fun. Our instructors are world class.",
+		"thumbnail" => "../assets/surf.png",
+	],
+	[
+		"heading" => "Fishing",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "../assets/fishing.png",
+	],
+	[
+		"heading" => "Get a Massage",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "../assets/massage.png",
+	],
+	[
+		"heading" => "Fishing",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "../assets/fishing.png",
+	],
+	[
+		"heading" => "Scuba Diving",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "../assets/scuba.png",
+	],
+];
