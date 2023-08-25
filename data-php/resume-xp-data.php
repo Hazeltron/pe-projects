4358cfed3fdf46b4ff10e786a796bd1794dd1aca
Hazeltron <paulhazelton.work@gmail.com>
2023-08-24 19:19:24 -0600

    Edited resume

--- data-php/resume-xp-data.php
+++ data-php/resume-xp-data.php
@@ -2,43 +2,37 @@
 
 $facts =[
 	[
-		"date" => "June?? - Present",
+		"date" => "June - Present",
 		"title" => "Web Design Student",
 		"location" => "Perpetual Education",
-		"description" => "I spent wayyy to much time on a computer having Derek Wood teach/correct my mistakes. It's fun though.",
+		"description" => "Under the mentorship of Derek Wood, I'm learning best practices in front end development and design. The program is six days a week, for nine months.",
 	],
 	[
 		"date" => "Aug 2023 - Present",
-		"title" => "Design Consultant",
+		"title" => "DESIGN CUNSOLTANT",
 		"location" => "Exterior-Visions, Colorado Springs, CO",
-		"description" => "Helped them find direction for site and sometimes answer design questions for them.",
+		"description" => "HI helped this small company find direction for their site and sometimes answer design questions for them.",
 	],
 	[
 		"date" => "June 2022 - Present",
-		"title" => "MAIN EXPO/SERVER",
+		"title" => "EXPO/SERVER",
 		"location" => "Jax Fish House and Oyster Bar, Colorado Springs, CO",
 		"description" => "I was required to have intimate knowledge of the menu, drinks and wine. I had a deep understanding of the steps of service and how to treat guests at an upscale restaurant. As a server and expo, I was able to juggle fast-paced and intricate tasks while remaining level-headed. On expo, I was required to catch mistakes and manage the kitchen and the wait staff while organizing orders and ensuring speedy ticket times.",
 	],
 	[
-		"date" => "Apr 2018 - Present",
+		"date" => "Apr 2018 - June 2022",
 		"title" => "K-12 ENGLISH TEACHER",
 		"location" => "Apollo English, Hanoi, Vietnam",
-		"description" => "Tasked with teaching, lesson planning, placement testing and performing demos for K-12 students. Capable of coordinating expectations between students, parents, administrators and other teachers to deliver lessons effectively. Responsible for carrying out the role of Head teacher while his boss was stuck in the Philippines due to COVID. During this time, enrollment at the school grew by nearly 50 percent.",
+		"description" => "I was tasked with teaching, lesson planning, placement testing and performing demos for K-12 students. I was highly capable of managing expectations between students, parents, administrators and other teachers to deliver lessons effectively. I was alsoesponsible for carrying out the role of Head teacher while my boss was stuck in the Philippines due to COVID. During this time, enrollment at the school grew by nearly 50 percent.",
 	],
 	[
-		"date" => "Mar 2016 - Jul 2018",
-		"title" => "MANAGER",
-		"location" => "Cheba Hut, Fort Collins, CO",
-		"description" => "Worked his way from server to manager. As a manager, was entrusted to keep the business running efficiently, assist co-workers with questions and business issues. Responsible for managing the daily operations of the restaurant and staff while complying with all health and safety regulations.",
+		"date" => "Apr 2017 - 2018",
+		"title" => "CONTENT WRITER/REPORTER",
+		"location" => "The Coloradoan and the Rooster Magazine, Fort Collins, Colorado",
+		"description" => "In this freelance role, I was tasked with finding, researching, interviewing and writing stories for The Rooster Magazine, a lifestyle publication. I also spent time as a copy writer for the advertising department of the Coloradoan and was asked to research, interview and report on buisnesses, industries and people. In both of these roles, I worked independantly and met deadlines while delivering a polished final product.",
 	],
-	[
-		"date" => "Apr 2009 - Sep 2014",
-		"title" => "BAR/BANQUET STAFF",
-		"location" => "Patty Jewett Golf Course Bar and Grill, Colorado Springs, CO",
-		"description" => "Rose quickly and consistently over the years in responsibilities by maintaining excellent customer service and sensitivity to golf culture and norms. Worked diligently and effectively to handle crunch times at high-traffic golf courses while managing the expectations of patrons. As part of his duties working in banquets, was responsible for setting up events including weddings and tournaments.",
-	],
-];
-
+]
+	
 
 
 
