00e10c75de888bf958ee32bb39524a8d4d887a6c
Hazeltron <paulhazelton.work@gmail.com>
2023-08-24 22:38:04 -0600

    Added content to Layout Garden

--- pages/layout-garden/index.php
+++ pages/layout-garden/index.php
@@ -21,7 +21,21 @@ include '../../header.php';
 	</div>
 </section>
 
-<?php include(getFile('modules/layout-cards/template.php')); ?>
+<section class="call-to-action">
+	<div class="inner-column">
+
+		<cta-inner>
+			
+			<text-area>
+			<h2 class="attention-voice">Book your Dream Vacation</h2>
+			<p>Western Airlines has flights to anywhere you can dream of. With the lowest fares and the most routes worldwide, we wan get you there. With Western you get two carry-ons on us and the most leg-room in the business. </p>
+			<a class="fly-button" href="#">FLY NOW</a>
+			</text-area>
+			
+		</cta-inner>
+		
+	</div>
+</section>
 
 
 <?php include '../../footer.php'; ?>
