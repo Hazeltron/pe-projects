a79af0c5208ffefc836a9249dd6781b439847028
Hazeltron <paulhazelton.work@gmail.com>
2023-08-26 15:01:55 -0600

    Added a codepen

--- pages/layout-garden/index.php
+++ pages/layout-garden/index.php
@@ -37,5 +37,20 @@ include '../../header.php';
 	</div>
 </section>
 
+<section class="beer-module">
+	<div class="inner-column">
+
+		<p class="codepen" data-height="489.296875" data-default-tab="html,result" data-slug-hash="LYXLVGL" data-editable="true" data-user="hazeltron" style="height: 489.296875px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+		  <span>See the Pen <a href="https://codepen.io/hazeltron/pen/LYXLVGL">
+		  Beer module</a> by paul hazelton (<a href="https://codepen.io/hazeltron">@hazeltron</a>)
+		  on <a href="https://codepen.io">CodePen</a>.</span>
+		</p>
+		<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+
+		<p>A reminder to finish this^</p>
+
+	</div>
+</section>
+
 
 <?php include '../../footer.php'; ?>
