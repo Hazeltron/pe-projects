352350a9a1007457e8a976376ee404a702a96a99
Hazeltron <paulhazelton.work@gmail.com>
2023-08-28 14:47:39 -0600

    updated cta

---
+++ modules/cta/template.php
@@ -0,0 +1,9 @@
+<cta-inner>
+			
+	<text-area>
+		<h2 class="attention-voice">Book your Dream Vacation</h2>
+		<p>Western Airlines has flights to anywhere you can dream of. With the lowest fares and the most routes worldwide, we wan get you there. With Western you get two carry-ons on us and the most leg-room in the business. </p>
+		<a class="fly-button" href="#">FLY NOW</a>
+	</text-area>
+			
+</cta-inner>
