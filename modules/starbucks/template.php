bbfc65a6d0a16e0dd4045cc76b0df31c3275326a
Hazeltron <paulhazelton.work@gmail.com>
2023-08-28 14:47:19 -0600

    Added starbucks module

---
+++ modules/starbucks/template.php
@@ -0,0 +1,20 @@
+	
+<starbucks-cta>
+
+	<picture class="coffee">
+		<img src="assets/starbucks.png" alt="Iced coffee with cream.">
+	</picture>
+
+	<text-content>
+
+		<h2 class="attention-voice">New Apple in the bunch</h2>
+		<p>Introducing our Iced Apple Crisp Oatmilk Shaken Espresso with a perfectly balanced taste.</p>
+		<a class="button" href="">Order Now</a>
+
+	</text-content>
+	
+</starbucks-cta>
+	
+	
+
+
