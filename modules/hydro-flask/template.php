c384c0f015ece8feeac9bbc715ac5734a3a8ed8f
Hazeltron <paulhazelton.work@gmail.com>
2023-08-29 20:27:27 -0600

    Added a hydro-flask module

--- modules/hydro-flask/template.php
+++ modules/hydro-flask/template.php
@@ -1,1 +1,50 @@
-template.php
+
+<hydro-flask-mod>
+
+<hydro-flask-grid>
+	<div class="bottle-1">
+		<picture class="bottles">
+			<img src="assets/hydro-1.png" alt="">
+		</picture>
+		<a href="">Bottles</a>
+	</div>
+
+	<div class="bottle-2">
+		<picture class="bottles">
+			<img src="assets/hydro-2.png" alt="">
+		</picture>
+		<a href="">Outdoor Kitchen</a>
+	</div>
+
+	<div class="bottle-3">
+		<picture class="bottles">
+			<img src="assets/hydro-3.png" alt="">
+		</picture>
+		<a href="">Accessories</a>
+	</div>
+
+	<div class="bottle-4">
+		<picture class="bottles">
+			<img src="assets/hydro-4.png" alt="">
+		</picture>
+		<a href="">New Colors</a>
+	</div>
+	
+</hydro-flask-grid>
+
+	<div class="main-image">
+
+		<picture class="girl">
+			<img src="../assets/girl-hydro.png" alt="">
+		</picture>
+
+		<text-area>
+			<p>32 oz & 40 oz with a handle and
+			straw to sip any which way.</p>
+			<a href="">Shop now</a>
+		</text-area>
+
+	</div>
+
+	
+</hydro-flask-mod>
