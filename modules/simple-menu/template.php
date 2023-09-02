f439245a53e717825e729a346345834c7f5c0b1d
Hazeltron <paulhazelton.work@gmail.com>
2023-09-02 01:09:44 -0600

    Updated layout-garden

---
+++ modules/simple-menu/template.php
@@ -0,0 +1,33 @@
+<simple-menu-contain>
+
+	<div class="press">
+
+		<h2 class="title">PRESS</h2>
+		<h2 class="name">DISFUTAR</h2>
+		
+	</div>
+
+	<div class="years">
+
+		<h3 class=" year">YEARS</h3>
+		<ul>
+			<li>
+				<a class="date" href="https://www.disfrutarbarcelona.com/?lang=en">2023</a>
+			</li>
+			<li>
+				<a class="date" href="">2022</a>
+			</li>
+			<li>
+				<a class="date" href="">2021</a>
+			</li>
+			<li>
+				<a class="date" href="">2020</a>
+			</li>
+			<li>
+				<a class="date" href="">2019</a>
+			</li>
+		</ul>
+
+	</div>
+
+</simple-menu-contain>
