a9ff42f4d220b5e4586e916a354e76a4008141ee
Hazeltron <paulhazelton.work@gmail.com>
2023-09-02 01:09:04 -0600

    deleted beer module

--- pages/layout-garden/index.php
+++ pages/layout-garden/index.php
@@ -13,21 +13,7 @@ include '../../header.php';
 </section>
 
 
-<section class="cards">
-	<div class="inner-column">
 
-		<?php include(getFile('modules/layout-cards/template.php')); ?>
-		
-	</div>
-</section>
-
-<section class="call-to-action">
-	<div class="inner-column">
-
-		<?php include(getFile('modules/cta/template.php')); ?>
-		
-	</div>
-</section>
 
 <section class="starbucks pumpkin">
 	<div class="inner-column">
@@ -71,18 +57,27 @@ include '../../header.php';
 	</div>
 </section>
 
-<section class="beer-module">
+<section class="disfutar">
+	<div class="inner-column">
+		
+		<?php include(getFile('modules/simple-menu/template.php')); ?>
+		
+	</div>
+</section>
+
+<section class="cards">
 	<div class="inner-column">
 
-		<p class="codepen" data-height="489.296875" data-default-tab="html,result" data-slug-hash="LYXLVGL" data-editable="true" data-user="hazeltron" style="height: 489.296875px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-		  <span>See the Pen <a href="https://codepen.io/hazeltron/pen/LYXLVGL">
-		  Beer module</a> by paul hazelton (<a href="https://codepen.io/hazeltron">@hazeltron</a>)
-		  on <a href="https://codepen.io">CodePen</a>.</span>
-		</p>
-		<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+		<?php include(getFile('modules/layout-cards/template.php')); ?>
+		
+	</div>
+</section>
 
-		<p>A reminder to finish this^</p>
+<section class="cards">
+	<div class="inner-column">
 
+		<?php include(getFile('modules/paps/template.php')); ?>
+		
 	</div>
 </section>
 
