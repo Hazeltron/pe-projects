c91a3b63569c150bee22057041f60e257fab68b5
Hazeltron <paulhazelton.work@gmail.com>
2023-09-02 01:10:58 -0600

    Added php error functions

--- functions.php
+++ functions.php
@@ -1,11 +1,20 @@
 <?php 
-// function showErrors() {
-// 	ini_set('display_errors', 1);
-// 	ini_set('displayy_startup_errors', 1);
-// 	error_reporting(E_ALL);
-// }
-	
-// showErrors(); 1
+function showErrors() {
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
+showErrors();
+
+function format($variable) {
+	echo "<pre>";
+		echo "<code>";
+			echo print_r( $variable );
+		echo "</code>";
+	echo "</pre>";
+}
+
+format( $_POST );
 
 function getFile($path){
 	return dirname(__FILE__) . '/' . $path;
