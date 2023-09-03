91efdda936b8e857df04f908816298db0ce848c1
Hazeltron <paulhazelton.work@gmail.com>
2023-09-03 00:54:28 -0600

    Added tims to modules

---
+++ modules/timberland/template.php
@@ -0,0 +1,41 @@
+
+<timberland-container>
+	<image-grid>
+		<ul>
+			<li>
+				<picture class="walking-girl">
+					<img src="assets/tim-girl-walking.png" alt="">
+				</picture>
+			</li>
+			<li class="moody">
+				<picture >
+					<img src="assets/tim-moody-girl.png" alt="">
+				</picture>
+			</li>
+			<li >
+				<picture>
+					<img src="assets/tim-boots.png" alt="">
+				</picture>
+			</li>
+			<li >
+				<picture>
+					<img src="assets/tim-legs.png" alt="">
+				</picture>
+			</li>
+			<li class="stairs" >
+				<picture>
+					<img src="assets/tim-stairs-girl.png" alt="">
+				</picture>
+			</li>
+		</ul>
+
+
+	<text-content>
+		<h2 class>BE YOURSELF. BE FEARLESS.</h2>
+		<p>TAKE ON THE SEASON IN BOLD, VERSATILE LOOKS</p>
+		<a href="">SHOP NOW</a>
+	</text-content>
+
+		</image-grid>
+
+</timberland-container>									
