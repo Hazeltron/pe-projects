4f764e89b4309964069f23bf5938998f359e33f4
Hazeltron <paulhazelton.work@gmail.com>
2023-09-18 13:44:45 -0600

    Added js forms and js folder

---
+++ js-forms/hello-world.php
@@ -0,0 +1,26 @@
+<?php include('../header.php'); ?>
+
+<section>
+	<div class="inner-column">
+
+		<form method="Post">
+			<field class="text"	>
+
+				<label for="userName" class="calm-voice">What's your name?</label>
+				<input id="userName" type="text">
+
+				<button type="submit" value="submit">Enter</button>
+
+			</field>
+
+			<output></output>
+		</form>
+
+
+		
+	</div>
+</section>
+
+<script src="js/hello-world.js"></script>
+
+
---
+++ js/hello-world.js
@@ -0,0 +1,19 @@
+console.clear();
+
+
+
+const $form = document.querySelector('form');
+const $input = $form.querySelector('input');
+const $outlet = document.querySelector('output');
+
+$form.addEventListener('submit', function(event){
+	event.preventDefault();
+
+	if($input.value) {
+		var message = `Hi there, ${$input.value}, it's nice to meet you!`;
+		$outlet.innerHTML = `<p>${message}</p>`;
+	} else {
+		message = `Please enter a name.`
+		$outlet.innerHTML = `<p>${message}</p>`;
+	}
+});
