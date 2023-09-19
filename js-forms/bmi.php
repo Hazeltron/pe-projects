b9f8e3f1ee823a763e67e79709c89e03e0a671b8
Hazeltron <paulhazelton.work@gmail.com>
2023-09-19 15:17:31 -0600

    Added js and php for bmi calculator form

---
+++ js-forms/bmi.php
@@ -0,0 +1,44 @@
+
+
+<?php include('../header.php'); ?>
+
+<section>
+	<div class="inner-column">
+
+		<form method="Post">
+			<field class="bmi-calc">
+
+				<label for="feet" class="calm-voice">How tall are you in feet?</label>
+				<input type="range" id="feet" min="0" max="10"  step="1">
+				<label id="label-feet" for="feet">
+					
+				</label>
+
+				<label for="inches" class="calm-voice">How tall are you in inches?</label>
+				<input type="range" id="inches" min="0" max="12"  step="1">
+				<label id="label-inches" for="inches">
+					
+				</label>
+				
+
+				<label for="weight" class="calm-voice">Your weight in pounds:</label>
+				<input id="weight" type="number">
+				
+				
+				
+
+
+
+				<button type="submit" value="submit">Enter</button>
+
+			</field>
+
+			<output></output>
+		</form>
+
+
+		
+	</div>
+</section>
+
+<script src="js/bmi.js"></script>
