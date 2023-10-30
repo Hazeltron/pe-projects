dcba7e6c7882a0f4977f638ae143864412e18e46
Hazeltron <paulhazelton.work@gmail.com>
2023-10-30 17:55:36 -0600

    Added Montana spray paint to layout garden

---
+++ modules/montana-cans/template.php
@@ -0,0 +1,32 @@
+<section>
+    <h1 class="attention-voice">LATEST PRODUCTS</h1>
+
+    <div class='cans-container'>
+
+        <div class="marble">
+            <picture>
+                <img src="https://www.montana-cans.com/media/54/24/5b/1580808032/MO-BLACK_400ML55363cf0cb91f.jpg" alt="">
+            </picture>
+            <p class="calm-voice">Montana BLACK 400ml</p>
+        </div>
+
+        <div class="glitter">
+            <picture>
+                <img src="https://www.montana-cans.com/media/37/db/00/1619520287/MONTANA-RUST-EFFECT-400ML.jpg" alt="">
+            </picture>
+            <p class="calm-voice">Montana RUST 400ml</p>
+        </div>
+
+        <div class="glass">
+            <picture>
+                <img src="https://www.montana-cans.com/media/04/c5/61/1580808006/MONTANA-VINTAGE-EFFECT_400ML-02.jpg" alt="">
+            </picture>
+            <p class="calm-voice">Montana VINTAGE 400ml</p>
+        </div>
+    
+
+    
+
+    </div>
+
+</section>
