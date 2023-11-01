356c06fbe93e2b7e84d745df94e01a0cf3a051ca
Hazeltron <paulhazelton.work@gmail.com>
2023-11-01 13:30:29 -0600

    Added blurb to layout intro

--- pages/layout-garden/index.php
+++ pages/layout-garden/index.php
@@ -4,12 +4,22 @@ include '../../header.php';
 ?>
 			
 
-<section>
-	<div class="inner-column">
+<section class="layout-intro">
+	<div class="inner-column layout-intro-content">
 
 		<h1 class="loud-voice">Layout Garden</h1>
-		<p>What is a layout garden and why have one? <br>
+		<p>At it’s core a layout garden is an area designed to showcase layouts. It’s meant to impress people.<br><br> More than that, however, it’s a testing ground used to experiment and learn. <br> 
+		In my case, I’ve been interested in how designers and developers make choices at big name brands when implementing smaller, more modularized layouts such as CTAs and marketing materials. <br><br> 
+		In other words, this is a school, a playground and a challenge arena rolled up in one. <br> 
+		Each of these layouts are designed to be highly responsive and fun. So, take a look around!
 		</p>
+
+		<div class="layout-links">
+			<h2 class="strong-voice">Find a Project</h2>
+			<ul>
+				<li><a href="#inner-link">Starbucks</a></li>
+			</ul>
+		</div>
 		
 	</div>
 </section>
@@ -23,7 +33,7 @@ include '../../header.php';
 		<section class="case-study">
 			<div class="inner-column">
 
-				<h2 class="attention-voice"><?=$layout['name']?></h2>
+				<h2 class="attention-voice" id="<?=$layout['id']?>"><?=$layout['name']?></h2>
 				<button class="calm-strong-voice button <?=$layout['button']?>">View Case Study</button>
 
 				<div class="case-study-content" data-target="<?=$layout['button']?>">
@@ -61,7 +71,7 @@ include '../../header.php';
 <section class="case-study">
 			<div class="inner-column">
 
-				<h2 class="attention-voice">Starbucks</h2>
+				<h2 class="attention-voice" id="inner-link">Starbucks</h2>
 				<a class="calm-strong-voice" href="https://www.starbucks.com/">Source</a>
 				<button class="calm-strong-voice button starbucks-button">View Case Study</button>
 
@@ -81,7 +91,7 @@ include '../../header.php';
 
 <section class="starbucks pumpkin">
 	<div class="inner-column">
-
+		
 		
 	<?php  
 			$heading = "Tap for pumpkin";
