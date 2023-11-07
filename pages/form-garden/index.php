81396316ed2bff40a5559980baba5399b5af6ca7
Hazeltron <paulhazelton.work@gmail.com>
2023-11-07 14:21:19 -0700

    Initialized form garden

---
+++ pages/form-garden/index.php
@@ -0,0 +1,29 @@
+<?php 
+include '../../header.php';
+
+?>
+			
+
+<section class="form-intro">
+	<div class="inner-column form-intro-content">
+
+		<h1 class="loud-voice">Form Garden</h1>
+		<p>At it’s core a layout garden is an area designed to showcase layouts. It’s meant to impress people.</p>
+
+		<div class="form-links">
+			<h2 class="strong-voice">Find a Forms</h2>
+			<ul>
+				<li><a href="#inner-link" class="calm-strong-voice">Starbucks</a></li>
+			</ul>
+		</div>
+    
+    </div>
+<section>
+
+<section>
+    <inner-column>
+        <?php include('../../js-forms/bmi.php')?>
+    </inner-column>
+</section>
+
+<?php include '../../footer.php'; ?>
