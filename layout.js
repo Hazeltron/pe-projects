23f77db93515208ec677756b33d3c82106dec2f3
Hazeltron <paulhazelton.work@gmail.com>
2023-11-07 14:25:34 -0700

    Added function to toggle dev tools in layout garden

--- layout.js
+++ layout.js
@@ -46,3 +46,57 @@ starbucksButton.addEventListener('click', function() {
 
 
 
+const vacationWire = document.querySelector('.layouts[data-wire="vacation"]');
+const hydroWire = document.querySelector('.layouts[data-wire="hydro"]');
+const disWire = document.querySelector('.layouts[data-wire="dis"]');
+const timlandWire = document.querySelector('.layouts[data-wire="timland"]');
+const montanaWire = document.querySelector('.layouts[data-wire="cans"]');
+const starWire = document.querySelector('.layouts[data-wire="starbucks-content"]');
+const vacationButtonWire = document.querySelector('.vacation-wire');
+const hydroButtonWire = document.querySelector('.hydro-wire');
+const disButtonWire = document.querySelector('.dis-wire');
+const timlandButtonWire = document.querySelector('.timland-wire');
+const montanaButtonWire = document.querySelector('.cans-wire');
+const starbucksButtonWire = document.querySelector('.starbucks-button-wire');
+
+
+function toggleBorder(layout) {
+    layout.classList.toggle('bordered');
+}
+
+vacationButtonWire.addEventListener('click', function() {
+    toggleBorder(vacationWire);
+});
+
+hydroButtonWire.addEventListener('click', function() {
+    toggleBorder(hydroWire);
+});
+
+disButtonWire.addEventListener('click', function() {
+    toggleBorder(disWire);
+});
+
+timlandButtonWire.addEventListener('click', function() {
+    toggleBorder(timlandWire);
+});
+
+montanaButtonWire.addEventListener('click', function() {
+    toggleBorder(montanaWire);
+});
+
+starbucksButtonWire.addEventListener('click', function() {
+    toggleBorder(starWire);
+});
+
+
+
+const screenSizeSlider = document.querySelector('#screen-size-slider');
+const vacationScreen = document.querySelector('.screen[data-screen="vacation"]');
+const hydroScreen = document.querySelector('.screen[data-screen="hydro"]');
+const disScreen = document.querySelector('.screen[data-screen="dis"]');
+const timlandScreen = document.querySelector('.screen[data-screen="timland"]');
+const montanaScreen = document.querySelector('.screen[data-screen="cans"]');
+const starScreen = document.querySelector('.screen[data-screen="starbucks-content"]');
+
+
+//when I move the slider I want the screen to change widths
