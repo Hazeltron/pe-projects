90ff7ad3d6d316e82c08ee83ee8758261b5a5bdc
Hazeltron <paulhazelton.work@gmail.com>
2023-11-09 12:24:21 -0700

    Finalized starbucks module

--- modules/starbucks/template.php
+++ modules/starbucks/template.php
@@ -1,26 +1,27 @@
 <?php 
-	$heading = $heading ?? "New apple in the bunch";
-	$paragraph = $paragraph ?? "Introducing our Iced Apple Crisp Oatmilk Shaken Espresso with a perfectly balanced taste.";
+	$heading = $heading ?? "Brewed with care";
+	$paragraph = $paragraph ?? "Since 2011, we've been brewing in the same place, with the same ingredients for the same crisp taste.";
+	$link = $link ?? "View Now";
 ?>	
 
 
 <starbucks-cta>
 
 	<picture class="coffee">
-		<img src="assets/starbucks.png" alt="Iced coffee with cream.">
+		<img src="assets/brewery.png" alt="Iced coffee with cream.">
 	</picture>
 
 	<text-content>
 
 		<h2><?=$heading?></h2>
 		<p><?=$paragraph?></p>
-		<a class="coffee-button" href="">Order Now</a>
+		<a class="coffee-button" href=""><?=$link?></a>
 
 	</text-content>
 	
 </starbucks-cta>
 
-<?php unset($heading, $paragraph); ?>
+<?php unset($heading, $paragraph, $link); ?>
 	
 	
 
