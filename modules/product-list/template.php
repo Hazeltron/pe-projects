70d4e1f77d6615c5050b148c7c652ff33cce3eb7
Hazeltron <paulhazelton.work@gmail.com>
2023-11-11 11:02:47 -0700

    initialized product list

---
+++ modules/product-list/template.php
@@ -0,0 +1,34 @@
+<section>
+    <inner-column>
+
+        <picture>
+            <img src="" alt="">
+        </picture>
+
+        <ul>
+            <li>
+                <picture>
+                    <img src="" alt="">
+                </picture>
+            </li>
+
+            <li>
+                <picture>
+                    <img src="" alt="">
+                </picture>
+            </li>
+        </ul>
+
+        <p>some text</p>
+
+        <picture>
+            <img src="" alt="">
+        </picture>
+        <p>item number</p>
+
+        <p>price</p>
+
+        <a href="">Add</a>
+
+    </inner-column>
+</section>
