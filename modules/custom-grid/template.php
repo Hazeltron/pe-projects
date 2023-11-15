e38590f7c98af12b91a8c3dae82611f60352edb6
Hazeltron <paulhazelton.work@gmail.com>
2023-11-15 00:24:58 -0700

    initialized hot girl grid

---
+++ modules/custom-grid/template.php
@@ -0,0 +1,29 @@
+<cool-girls>
+    <ul>
+        <li class="main">
+            <picture >
+                <img src="assets/girl-gap.png" alt="">
+            </picture>
+        </li>
+        <li class="hat">
+            <picture >
+                <img src="assets/girl-hat.png" alt="">
+            </picture>
+        </li>
+        <li class="hoodie">
+            <picture >
+                <img src="assets/girl-hoodie.png" alt="">
+            </picture>
+        </li>
+        <li class="jewels">
+            <picture >
+                <img src="assets/girl-jewels.png" alt="">
+            </picture>
+        </li>
+        <li class="shirt">
+            <picture >
+                <img src="assets/girl-shirt.png" alt="">
+            </picture>
+        </li>
+    </ul>
+</cool-girls>
