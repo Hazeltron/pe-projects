e670bb9c099983b8130222d4c579037d594672c6
Hazeltron <paulhazelton.work@gmail.com>
2023-11-15 11:37:33 -0700

    Updated journal

--- journal.php
+++ journal.php
@@ -1,7 +1,12 @@
 <section>
     <inner-column>
         <p>
-            
+        Yesterday:
+Got in a huddle with Ad and made some forms with Vue. That was fun.
+After that, I made a simple grid with these model photos that have been sitting in my assets folder. I want to make them into an apparel layout after I finish my product list layout.
+Today:
+I hooked up products list layout and girl apparel grid to my js controllers. I DO NOT LIKE how I set this up in js. it takes way too much time to update and there needs to be a simpler function controlling these. I actually thik that this whole loop that's controlling the layouts is badly set up and overly complicated.
+I'm going to try to finish the product list layout and then hop on with Derek and Ad to review some of the forms we did yesterday. Then hopefully do some more forms.
         </p>
     </inner-column>
 </section>
