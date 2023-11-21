4f5e6e37099efe0a053d8975531f81f435d0dd73
Hazeltron <paulhazelton.work@gmail.com>
2023-11-20 19:56:29 -0700

    updated journal

--- journal.php
+++ journal.php
@@ -8,5 +8,9 @@ Today:
 I hooked up products list layout and girl apparel grid to my js controllers. I DO NOT LIKE how I set this up in js. it takes way too much time to update and there needs to be a simpler function controlling these. I actually thik that this whole loop that's controlling the layouts is badly set up and overly complicated.
 I'm going to try to finish the product list layout and then hop on with Derek and Ad to review some of the forms we did yesterday. Then hopefully do some more forms.
         </p>
+
+        <p>
+    This Vue stuff is hard.
+        </p>
     </inner-column>
 </section>
