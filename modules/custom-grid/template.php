1ed8546d5a139eadcdef9da1225887caaf683a43
Hazeltron <paulhazelton.work@gmail.com>
2023-11-21 22:01:01 -0700

    commted full css for cool girls layout

--- modules/custom-grid/template.php
+++ modules/custom-grid/template.php
@@ -1,29 +1,67 @@
 <cool-girls>
-    <ul>
+    <ul class="main-grid">
+
         <li class="main">
-            <picture >
+            <picture>
                 <img src="assets/girl-gap.png" alt="">
             </picture>
+            <h2 class="loud-voice main-link">Take control <br> of your style</h2>
         </li>
+
         <li class="hat">
-            <picture >
-                <img src="assets/girl-hat.png" alt="">
+            <picture class="grey-hover">
+                <a href="#">
+                    <img src="assets/girl-hat.png" alt="">
+                </a>
             </picture>
+            <a href="#" class="attention-voice product-link">Hats</a>
         </li>
+
         <li class="hoodie">
-            <picture >
+            <picture class="grey-hover">
+                <a href="#">
                 <img src="assets/girl-hoodie.png" alt="">
+                </a>
             </picture>
+            <a href="#" class="attention-voice product-link">Hoodies</a>
         </li>
+
         <li class="jewels">
-            <picture >
+            <picture class="grey-hover">
+                <a href="#">
                 <img src="assets/girl-jewels.png" alt="">
+                </a>  
             </picture>
+            <a href="#" class="attention-voice product-link">Jewlery</a>
         </li>
+
         <li class="shirt">
-            <picture >
+            <picture class="grey-hover">
+                <a href="#">
                 <img src="assets/girl-shirt.png" alt="">
+                </a>
             </picture>
+            <a href="#" class="attention-voice product-link">Shirts</a>
         </li>
+
+        <!-- <li class="sub-grid">
+            <div class="title">
+                <h2 class="attention-voice">Hello</h2>
+            </div>
+            <div class="hoodie-link">
+                <h2 class="attention-voice">Hello</h2>
+            </div>
+            <div class="shirt-link">
+                <h2 class="attention-voice">Hello</h2>
+            </div>
+            <div class="jewels-link">
+                <h2 class="attention-voice">Hello</h2>
+            </div>
+            <div class="hat-link">
+                <h2 class="attention-voice">Hello</h2>
+            </div>
+        </div> -->
+
     </ul>
+   
 </cool-girls>
