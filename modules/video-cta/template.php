84b4dc08baad2ffb5a01950301a2bc2a3820ef11
Hazeltron <paulhazelton.work@gmail.com>
2023-11-27 21:20:40 -0700

    initialized video-cta module

---
+++ modules/video-cta/template.php
@@ -0,0 +1,14 @@
+<video-cta>
+    <video autoplay muted loop>
+    <source src="assets/video.mp4" type="video/mp4">
+    </video>
+    <text-content>
+        <div class="tiitle-tiff">
+        <h2 class="lay-loud-voice">A Tiffany Holiday Awaits</h2>
+        <p>Join Rosie Huntington-Whiteley as she captures the joy and magic of the festive season in the heart of New York City.</p>
+        <a href="#">Explore The story</a>
+
+        </div>
+       
+    </text-content>
+</video-cta>
