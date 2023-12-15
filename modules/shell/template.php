c5b12fe53c9c51a19977dca0f792e70a42df55c0
Hazeltron <paulhazelton.work@gmail.com>
2023-12-15 01:06:58 -0700

    Initialized shell layout

---
+++ modules/shell/template.php
@@ -0,0 +1,21 @@
+<div class="shell-container">
+
+    <ul>
+        <li>
+            <article>
+                <div class="shell-img">
+                    <picture>
+                        <img src="assets/landscape.jpg" alt="">
+                    </picture>
+                </div>
+
+                <div class="shell-text">
+                    <h2>Something</h2>
+                    <p>Something else</p>
+                    <a href="#">Something link</a>
+                </div>
+            </article>
+        </li>
+    </ul>
+
+</div>
