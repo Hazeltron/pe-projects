9041f17b1d4f8cf10bb31070186ac0bf04a054a7
Hazeltron <paulhazelton.work@gmail.com>
2023-12-18 22:20:48 -0700

    Reformated my html

--- modules/shell/template.php
+++ modules/shell/template.php
@@ -1,54 +1,53 @@
 <div class="upper-shell">
-
     <ul class="upper-grid">
-    <?php 
-      $json = file_get_contents('../../data-php/shell.json');
-      $cards = json_decode($json, true);
+        <?php 
+        $json = file_get_contents('../../data-php/shell.json');
+        $cards = json_decode($json, true);
 
-    foreach (array_slice($cards, 0, 5) as $card) { ?>
-        <li>
-            <article class="upper-cards">
-                <div class="shell-img">
-                    <picture>
-                        <img src="assets/<?= $card['img'] ?>" alt="">
-                    </picture>
-                </div>
+        foreach (array_slice($cards, 0, 5) as $card) { ?>
+            <li>
+                <article class="upper-cards">
+                    <a href="#">
+                        <div class="shell-img">
+                            <picture>
+                                <img src="assets/<?= $card['img'] ?>" alt="">
+                            </picture>
+                        </div>
 
-                <div class="shell-text">
-                    <h2 class="lay-attention-voice"><?= $card['title'] ?></h2>
-                    <p class="lay-calm-voice"><?= $card['text'] ?></p>
-                    <a href="#" class="lay-calm-voice"><?= $card['link'] ?></a>
-                </div>
-            </article>
-        </li>
-    <?php } ?>
+                        <div class="shell-text">
+                            <h2 class="lay-attention-voice"><?= $card['title'] ?></h2>
+                            <p class="lay-calm-voice"><?= $card['text'] ?></p>
+                            <a href="#" class="lay-calm-voice"><?= $card['link'] ?></a>
+                        </div>
+                    </a>
+                </article>
+            </li>
+        <?php } ?>
     </ul>
-
-    
-
 </div>
 
 <div class="lower-shell">
     <h2 class="lay-loud-voice">Featured content</h2>
     <ul class="lower-grid">
-            
         <?php 
         foreach (array_slice($cards, 5) as $card) { ?>
             <li>
                 <article class="lower-cards">
-                    <div class="shell-img">
-                        <picture>
-                            <img src="assets/<?= $card['img'] ?>" alt="">
-                        </picture>
-                    </div>
+                    <a href="#">
+                        <div class="shell-img">
+                            <picture>
+                                <img src="assets/<?= $card['img'] ?>" alt="">
+                            </picture>
+                        </div>
 
-                    <div class="shell-text">
-                        <h2 class="lay-attention-voice"><?= $card['title'] ?></h2>
-                        <p><?= $card['text'] ?></p>
-                        <a href="#"><?= $card['link'] ?></a>
-                    </div>
+                        <div class="shell-text">
+                            <h2 class="lay-attention-voice"><?= $card['title'] ?></h2>
+                            <p><?= $card['text'] ?></p>
+                            <a href="#"><?= $card['link'] ?></a>
+                        </div>
+                    </a>
                 </article>
             </li>
         <?php } ?>
-        </ul>
+    </ul>
 </div>
