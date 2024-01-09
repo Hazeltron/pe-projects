66338ba5bb3a28ce565beff222faf32975dd66cd
Hazeltron <paulhazelton.work@gmail.com>
2024-01-09 16:52:51 -0700

    hid studies and bad layouts

--- layout.js
+++ layout.js
@@ -1,76 +1,76 @@
-const vacationStudy = document.querySelector('.case-study-content[data-target="vacation"]');
-const hydroStudy = document.querySelector('.case-study-content[data-target="hydro"]');
-const disStudy = document.querySelector('.case-study-content[data-target="dis"]');
-const timlandStudy = document.querySelector('.case-study-content[data-target="timland"]');
-const montanaStudy = document.querySelector('.case-study-content[data-target="cans"]');
-const starStudy = document.querySelector('.case-study-content[data-target="starbucks-content"]');
-const productStudy = document.querySelector('.case-study-content[data-target="product-list"]');
-const coolGirlsStudy = document.querySelector('.case-study-content[data-target="girls"]');
-const videoCtaStudy = document.querySelector('.case-study-content[data-target="video"]');
-const gameSpotStudy = document.querySelector('.case-study-content[data-target="game-spot-button"]');
+// const vacationStudy = document.querySelector('.case-study-content[data-target="vacation"]');
+// const hydroStudy = document.querySelector('.case-study-content[data-target="hydro"]');
+// const disStudy = document.querySelector('.case-study-content[data-target="dis"]');
+// const timlandStudy = document.querySelector('.case-study-content[data-target="timland"]');
+// const montanaStudy = document.querySelector('.case-study-content[data-target="cans"]');
+// const starStudy = document.querySelector('.case-study-content[data-target="starbucks-content"]');
+// const productStudy = document.querySelector('.case-study-content[data-target="product-list"]');
+// const coolGirlsStudy = document.querySelector('.case-study-content[data-target="girls"]');
+// const videoCtaStudy = document.querySelector('.case-study-content[data-target="video"]');
+// const gameSpotStudy = document.querySelector('.case-study-content[data-target="game-spot-button"]');
 
-const vacationButton = document.querySelector('.vacation');
-const hydroButton = document.querySelector('.hydro');
-const disButton = document.querySelector('.dis');
-const timlandButton = document.querySelector('.timland');
-const montanaButton = document.querySelector('.cans');
-const starbucksButton = document.querySelector('.starbucks-button');
-const productButton = document.querySelector('.product-list');
-const coolGirlsButton = document.querySelector('.girls');
-const videoCtaButton = document.querySelector('.video');
-const gameSpotButton = document.querySelector('.game-spot-button');
+// const vacationButton = document.querySelector('.vacation');
+// const hydroButton = document.querySelector('.hydro');
+// const disButton = document.querySelector('.dis');
+// const timlandButton = document.querySelector('.timland');
+// const montanaButton = document.querySelector('.cans');
+// const starbucksButton = document.querySelector('.starbucks-button');
+// const productButton = document.querySelector('.product-list');
+// const coolGirlsButton = document.querySelector('.girls');
+// const videoCtaButton = document.querySelector('.video');
+// const gameSpotButton = document.querySelector('.game-spot-button');
 
-function displayContent(content) {
-    if (content.style.display !== 'block') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-}
+// function displayContent(content) {
+//     if (content.style.display !== 'block') {
+//         content.style.display = 'block';
+//     } else {
+//         content.style.display = 'none';
+//     }
+// }
 
-vacationButton.addEventListener('click', function() {
-    displayContent(vacationStudy);
-});
+// vacationButton.addEventListener('click', function() {
+//     displayContent(vacationStudy);
+// });
 
-hydroButton.addEventListener('click', function() {
-    displayContent(hydroStudy);
-});
+// hydroButton.addEventListener('click', function() {
+//     displayContent(hydroStudy);
+// });
 
-disButton.addEventListener('click', function() {
-    displayContent(disStudy);
-});
+// disButton.addEventListener('click', function() {
+//     displayContent(disStudy);
+// });
 
-timlandButton.addEventListener('click', function() {
-    displayContent(timlandStudy);
-});
+// timlandButton.addEventListener('click', function() {
+//     displayContent(timlandStudy);
+// });
 
-montanaButton.addEventListener('click', function() {
-    displayContent(montanaStudy);
-});
+// montanaButton.addEventListener('click', function() {
+//     displayContent(montanaStudy);
+// });
 
-starbucksButton.addEventListener('click', function() {
-    displayContent(starStudy);
-});
-//doesnt work below
-productButton.addEventListener('click', function() {
-    displayContent(productStudy);
-});
-coolGirlsButton.addEventListener('click', function() {
-    displayContent(coolGirlsStudy);
-});
-videoCtaButton.addEventListener('click', function() {
-    displayContent(videoCtaStudy);
-});
-gameSpotButton.addEventListener('click', function() {
-    displayContent(gameSpotStudy);
-});
+// starbucksButton.addEventListener('click', function() {
+//     displayContent(starStudy);
+// });
+// //doesnt work below
+// productButton.addEventListener('click', function() {
+//     displayContent(productStudy);
+// });
+// coolGirlsButton.addEventListener('click', function() {
+//     displayContent(coolGirlsStudy);
+// });
+// videoCtaButton.addEventListener('click', function() {
+//     displayContent(videoCtaStudy);
+// });
+// gameSpotButton.addEventListener('click', function() {
+//     displayContent(gameSpotStudy);
+// });
 
 
 
 
 
 const vacationWire = document.querySelector('.layouts[data-wire="vacation"]');
-const hydroWire = document.querySelector('.layouts[data-wire="hydro"]');
+// const hydroWire = document.querySelector('.layouts[data-wire="hydro"]');
 const disWire = document.querySelector('.layouts[data-wire="dis"]');
 const timlandWire = document.querySelector('.layouts[data-wire="timland"]');
 const montanaWire = document.querySelector('.layouts[data-wire="cans"]');
@@ -81,7 +81,7 @@ const videoCtaWire = document.querySelector('.layouts[data-wire="video"]');
 const gameSpotWire = document.querySelector('.layouts[data-wire="game-spot-button"]');
 
 const vacationButtonWire = document.querySelector('.vacation-wire');
-const hydroButtonWire = document.querySelector('.hydro-wire');
+// const hydroButtonWire = document.querySelector('.hydro-wire');
 const disButtonWire = document.querySelector('.dis-wire');
 const timlandButtonWire = document.querySelector('.timland-wire');
 const montanaButtonWire = document.querySelector('.cans-wire');
@@ -99,9 +99,9 @@ vacationButtonWire.addEventListener('click', function() {
     toggleBorder(vacationWire);
 });
 
-hydroButtonWire.addEventListener('click', function() {
-    toggleBorder(hydroWire);
-});
+// hydroButtonWire.addEventListener('click', function() {
+//     toggleBorder(hydroWire);
+// });
 
 disButtonWire.addEventListener('click', function() {
     toggleBorder(disWire);
@@ -137,7 +137,7 @@ gameSpotButtonWire.addEventListener('click', function() {
 
 const rangeSliderStar = document.querySelector('#range');
 const rangeSliderCards = document.querySelector('#cards-range');
-const rangeSliderHydro = document.querySelector('#hydro-range');
+// const rangeSliderHydro = document.querySelector('#hydro-range');
 const rangeSliderDis = document.querySelector('#dis-range');
 const rangeSliderTimland = document.querySelector('#timland-range');
 const rangeSliderCans = document.querySelector('#cans-range');
@@ -148,7 +148,7 @@ const rangeSliderGameSpot = document.querySelector('#game-spot');
 
 
 const vacationScreen = document.querySelector('.screen[data-screen="vacation"]');
-const hydroScreen = document.querySelector('.screen[data-screen="hydro"]');
+// const hydroScreen = document.querySelector('.screen[data-screen="hydro"]');
 const disScreen = document.querySelector('.screen[data-screen="dis"]');
 const timlandScreen = document.querySelector('.screen[data-screen="timland"]');
 const montanaScreen = document.querySelector('.screen[data-screen="cans"]');
@@ -179,7 +179,7 @@ function setupSlider(sliderId, screenDataAttr) {
 
 setupSlider('range', 'starbucks-content');
 setupSlider('cards-range', 'vacation');
-setupSlider('hydro-range', 'hydro');
+// setupSlider('hydro-range', 'hydro');
 setupSlider('dis-range', 'dis');
 setupSlider('timland-range', 'timland');
 setupSlider('cans-range', 'cans');
