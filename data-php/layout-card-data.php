cccb63a5d03f12cadc29b100a4f013e1c09fe5b0
Hazeltron <paulhazelton.work@gmail.com>
2024-01-09 20:12:39 -0700

    Added content to layout-cards data

--- data-php/layout-card-data.php
+++ data-php/layout-card-data.php
@@ -4,33 +4,33 @@
 
 $articles = [
 	[
-		"heading" => "Scuba Diving",
-		"description" => "Come check out the coral reefs and fish and things. Lorem ipsum dolor sit amet.",
-		"thumbnail" => "../assets/scuba.png",
+		"heading" => "Luxury Scuba Diving Adventure",
+		"description" => "Dive into the depths with a private scuba experience, exploring vibrant marine life in the crystal-clear waters of your own aquatic playground.",
+		"thumbnail" => "scuba.png",
 	],
 	[
-		"heading" => "Surfing",
-		"description" => "Come surf with us. It would be a lot of fun. Our instructors are world class.",
-		"thumbnail" => "../assets/surf.png",
+		"heading" => "Private Surfing Experience",
+		"description" => "Ride the waves in an exclusive surfing lesson guided by experts, creating unforgettable memories on the pristine shores.",
+		"thumbnail" => "surf.png",
 	],
 	[
-		"heading" => "Fishing",
-		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "../assets/fishing.png",
+		"heading" => "Exclusive Fishing Excursion",
+		"description" => "Embark on a private fishing journey, guided by seasoned anglers, as you cast your line into the abundant waters for a day of indulgent tranquility.",
+		"thumbnail" => "fishing.png",
 	],
 	[
-		"heading" => "Get a Massage",
-		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "../assets/massage.png",
+		"heading" => "Indulgent Beachside Massage",
+		"description" => "Surrender to serenity with a beachside massage, where the rhythmic sounds of waves complement the expert touch of skilled therapists.",
+		"thumbnail" => "massage.png",
 	],
 	[
-		"heading" => "Fishing",
-		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "../assets/fishing.png",
+		"heading" => "Sunset Champagne Cruise",
+		"description" => "Sail into evening's golden hues, champagne in hand, as the sun dips below the horizon—a sensory symphony of luxury on the high seas.",
+		"thumbnail" => "cruise.png",
 	],
 	[
-		"heading" => "Scuba Diving",
-		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "../assets/scuba.png",
+		"heading" => "Private Beachside Dinner",
+		"description" => "Savor a moonlit soirée on the shore, where a personal chef crafts a bespoke menu, waves providing the soundtrack to an intimate culinary journey.",
+		"thumbnail" => "beach-dinner.png",
 	],
 ];
