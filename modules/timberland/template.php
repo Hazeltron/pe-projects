2eb74ecfba7571328d75266b44f281b97ff0ab8d
Hazeltron <paulhazelton.work@gmail.com>
2024-01-19 22:12:36 -0700

    Fixed timberland grid

--- modules/timberland/template.php
+++ modules/timberland/template.php
@@ -1,41 +1,41 @@
-
 <timberland-container>
-	<image-grid>
-		<ul>
-			<li>
-				<picture class="walking-girl">
-					<img src="assets/tim-girl-walking.png" alt="">
-				</picture>
-			</li>
-			<li class="moody">
-				<picture >
-					<img src="assets/tim-moody-girl.png" alt="">
-				</picture>
-			</li>
-			<li >
-				<picture>
-					<img src="assets/tim-boots.png" alt="">
-				</picture>
-			</li>
-			<li >
-				<picture>
-					<img src="assets/tim-legs.png" alt="">
-				</picture>
-			</li>
-			<li class="stairs" >
-				<picture>
-					<img src="assets/tim-stairs-girl.png" alt="">
-				</picture>
-			</li>
-		</ul>
+  <image-grid>
+    <ul>
+      <li>
+        <picture class="walking-girl">
+          <img src="assets/tim-girl-walking.png" alt="">
+        </picture>
+      </li>
+      <li class="moody">
+        <picture>
+          <img src="assets/tim-moody-girl.png" alt="">
+        </picture>
+      </li>
 
+	  
+      <li>
+        <picture>
+          <img src="assets/tim-boots.png" alt="">
+        </picture>
+      </li>
+      <li>
+        <picture>
+          <img src="assets/tim-legs.png" alt="">
+        </picture>
+      </li>
+      <li class="stairs">
+        <picture>
+          <img src="assets/tim-stairs-girl.png" alt="">
+        </picture>
+      </li>
+    </ul>
 
-	<text-content>
-		<h2 class="attention-voice">BE YOURSELF. <br> BE FEARLESS.</h2>
-		<p>TAKE ON THE SEASON IN BOLD, VERSATILE LOOKS</p>
-		<a href="">SHOP NOW</a>
-	</text-content>
+    <text-content>
+    <h2 class="attention-voice">BE YOURSELF. <br> BE FEARLESS.</h2>
+    <p>TAKE ON THE SEASON IN BOLD, VERSATILE LOOKS</p>
+    <a href="">SHOP NOW</a>
+  </text-content>
+  </image-grid>
 
-		</image-grid>
-
-</timberland-container>									
+  
+</timberland-container>							
