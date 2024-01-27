c720ab84bd703391bc743c3eee9882668cd97c3c
Hazeltron <paulhazelton.work@gmail.com>
2024-01-27 15:20:48 -0700

    initialized resume

---
+++ pages/resume/index.php
@@ -0,0 +1,64 @@
+<?php 
+include '../../header.php';
+
+?>
+
+
+    <inner-column>
+    <page-wrapper>
+
+    
+
+<main-resume>
+    <section class="resume-title">
+        <inner-column>
+            <h1 class="loud-voice">Paul Hazelton</h1>
+
+            <ul>
+                <li>
+                    <a href="https://github.com/Hazeltron" class="medium-voice">GitHub</a>
+                </li>
+                <li>
+                    <a href="http://www.linkedin.com/in/paul-hazelton-b35843116" class="medium-voice">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://codepen.io/hazeltron/pens/public" class="medium-voice">CodePen</a>
+                </li>
+            </ul>
+        </inner-column>
+    </section>
+
+    <section class="experience">
+        <inner-column>
+            <h2 class="attention-voice">Experience</h2>
+            <h3 class="strong-voice">Web Development & Design Mentorship <span class="medium-voice">-- Student</span></h3>
+            <h4 class="calm-strong-voice">June 2022  - present</h4>
+            <ul>
+                <li class="calm-voice">Studied design and web development 7 days a week, 4-6 hours a day for 9 months.
+                </li>
+                <li class="calm-voice">Learned how to create static webpages, server- and client-side applications with HTML, CSS, JavaScript, Vue and PHP.
+                </li>
+                <li class="calm-voice">Designed and created a content management system and to manage my website using PHP, HTML, CSS, and JS.
+                </li>
+                <li class="calm-voice">Created a design system to manage reusable components and modules, typography and site-wide conventions; programmatically built a screen size adjuster, wireframe toggler and scroll bar.
+                </li>
+            </ul>
+        </inner-column>
+    </section>
+
+</main-resume>
+
+
+<aside>
+
+        <h2>hello</h2>
+
+    
+</aside>
+</page-wrapper>
+</inner-column>
+
+
+
+
+<?php include '../../footer.php'; ?>
