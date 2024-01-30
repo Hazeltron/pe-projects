274ae05365a3a482cf1facf41ab3fc09281e02a6
Hazeltron <paulhazelton.work@gmail.com>
2024-01-30 15:57:23 -0700

    initialized resume html and php

--- pages/resume/index.php
+++ pages/resume/index.php
@@ -1,64 +1,82 @@
 <?php 
 include '../../header.php';
-
 ?>
 
-
-    <inner-column>
+<inner-column>
     <page-wrapper>
 
-    
+        <main-resume>
+            <section class="resume-title">
+                <inner-column>
+                    <h1 class="loud-voice">Paul Hazelton</h1>
 
-<main-resume>
-    <section class="resume-title">
-        <inner-column>
-            <h1 class="loud-voice">Paul Hazelton</h1>
+                    <ul>
+                        <li><a href="https://github.com/Hazeltron" class="medium-voice">GitHub</a></li>
+                        <li><a href="http://www.linkedin.com/in/paul-hazelton-b35843116" class="medium-voice">LinkedIn</a></li>
+                        <li><a href="https://codepen.io/hazeltron/pens/public" class="medium-voice">CodePen</a></li>
+                    </ul>
+                </inner-column>
+            </section>
 
-            <ul>
-                <li>
-                    <a href="https://github.com/Hazeltron" class="medium-voice">GitHub</a>
-                </li>
-                <li>
-                    <a href="http://www.linkedin.com/in/paul-hazelton-b35843116" class="medium-voice">LinkedIn</a>
-                </li>
-                <li>
-                    <a href="https://codepen.io/hazeltron/pens/public" class="medium-voice">CodePen</a>
-                </li>
-            </ul>
-        </inner-column>
-    </section>
+            <section class="experience">
+                <inner-column>
+                    <h2 class="attention-voice">Experience</h2>
 
-    <section class="experience">
-        <inner-column>
-            <h2 class="attention-voice">Experience</h2>
-            <h3 class="strong-voice">Web Development & Design Mentorship <span class="medium-voice">-- Student</span></h3>
-            <h4 class="calm-strong-voice">June 2022  - present</h4>
-            <ul>
-                <li class="calm-voice">Studied design and web development 7 days a week, 4-6 hours a day for 9 months.
-                </li>
-                <li class="calm-voice">Learned how to create static webpages, server- and client-side applications with HTML, CSS, JavaScript, Vue and PHP.
-                </li>
-                <li class="calm-voice">Designed and created a content management system and to manage my website using PHP, HTML, CSS, and JS.
-                </li>
-                <li class="calm-voice">Created a design system to manage reusable components and modules, typography and site-wide conventions; programmatically built a screen size adjuster, wireframe toggler and scroll bar.
-                </li>
-            </ul>
-        </inner-column>
-    </section>
+                    <?php 
+                    $json = file_get_contents('../../data-php/resume.json');
+                    $details = json_decode($json, true);
 
-</main-resume>
+                    foreach ($details as $detail) { ?> 
+                        <job-detail>
+                            <h3 class="strong-voice"><?= $detail['jobTitle'] ?> <span class="medium-voice"><?= $detail['role'] ?></span></h3>
+                            <h4 class="calm-strong-voice"><?= $detail['date'] ?></h4>
+                            <ul class="calm-voice">
+                                <?php foreach ($detail['description'] as $item) { ?>
+                                    <?= $item ?>
+                                <?php } ?>
+                            </ul>
+                        </job-detail>
+                        
+                    <?php } ?>
+                   
+                </inner-column>
+            </section>
 
+        </main-resume>
 
-<aside>
+        <aside>
+            <detail-wrapper>
 
-        <h2>hello</h2>
+                <contact>
+                    <h3 class="calm-strong-voice">Contact</h3>
+                    <p class="calm-voice">🏔️ Colorado Springs, Co. 80907</p>
+                    <p class="calm-voice">📱 (719)337-0666</p>
+                    <p class="calm-voice">📬 paulhazelton.work@gmail.com</p>
+                </contact>
 
-    
-</aside>
-</page-wrapper>
+                <skills>
+                    <h3 class="calm-strong-voice">Skills & ToolKit</h3>
+                    <p>Figma, Affinity Designer</p>
+                    <p>Visual Design | Design Systems</p>
+                    <p>User Testing</p>
+                    <p>Familiarity with CMS</p>
+                    <p>Collaborative mindset with designers and advertisers</p>
+                </skills>
+
+                <languages>
+                    <h3 class="calm-strong-voice">Languages & Frameworks</h3>
+                    <p>HTML5 | CSS3 | JavaScript | PHP | Vue</p>
+                </languages>
+
+                <education>
+                    <h3 class="calm-strong-voice">Education</h3>
+                    <p>Perpetual Education, Remote</p>
+                    <p>Colorado State University, Fort Collins, Co. — Journalism</p>
+                </education>
+
+            </detail-wrapper>
+        </aside>
+    </page-wrapper>
 </inner-column>
-
-
-
 
 <?php include '../../footer.php'; ?>
