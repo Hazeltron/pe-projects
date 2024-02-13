d4843b6ef23380da06c25c80c73e84208ca4d813
Hazeltron <paulhazelton.work@gmail.com>
2024-02-12 20:24:41 -0700

    Initialized mubasic

---
+++ modules/mubasic/template.php
@@ -0,0 +1,37 @@
+<mubasic-wrapper>
+
+    <article>
+
+        <header>
+            <a href="#" class="lay-calm-voice">mubasic</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 356 373" fill="none">
+            <path d="M248 9.53571C248 4.26928 252.269 0 257.536 0H346.464C351.731 0 356 4.26928 356 9.53571V114.464C356 119.731 351.731 124 346.464 124H257.536C252.269 124 248 119.731 248 114.464V9.53571Z" fill="#1B1A19"></path>
+            <rect y="139.996" width="108" height="123.964" rx="9.53571" fill="#1B1A19"></rect>
+            <rect x="124" y="62.1211" width="108" height="309.911" rx="9.53571" fill="#1B1A19"></rect>
+            <rect x="248" y="139.996" width="108" height="123.964" rx="9.53571" fill="#1B1A19"></rect>
+        </svg>
+        </header>
+
+        <h2 class="lay-louder-voice">The <span>#1</span> source for children's music.</h2>
+
+        <footer>
+            <p>An ever-growing catalogue of high-quality children’s music, sourced by talented music creators from all over the world.</p>
+        </footer>
+            
+    </article>
+
+    
+
+    <aside>
+        <ul>
+            <li><p>m</p></li>
+            <li><p>u</p></li>
+            <li><p>b</p></li>
+            <li><p>a</p></li>
+            <li><p>s</p></li>
+            <li><p>i</p></li>
+            <li><p>c</p></li>
+        </ul>
+    </aside>
+
+</mubasic-wrapper>
