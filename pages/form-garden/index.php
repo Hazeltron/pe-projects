7776a3b9d3a13fd33ad3ccb62994a1059b257eac
Hazeltron <paulhazelton.work@gmail.com>
2024-02-14 13:24:50 -0700

    Reordered forms

--- pages/form-garden/index.php
+++ pages/form-garden/index.php
@@ -22,6 +22,18 @@ include '../../header.php';
 
 <section>
     <inner-column>
+        <?php include('../../php-forms/exchange-rate.php')?>
+    </inner-column>
+</section>
+
+<section>
+    <inner-column>
+        <?php include('../../php-forms/password.php')?>
+    </inner-column>
+</section>
+
+<section>
+    <inner-column>
         <?php include('../../js-forms/password-strength.php')?>
     </inner-column>
 </section>
@@ -33,11 +45,44 @@ include '../../header.php';
 </section>
 
 
+
 <section>
     <inner-column>
-        <?php include('../../php-forms/password.php')?>
+	<p class="codepen" data-height="747.265625" data-default-tab="result" data-slug-hash="MWLOLMB" data-editable="true" data-user="hazeltron" style="height: 747.265625px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/hazeltron/pen/MWLOLMB">
+  Checkout</a> by paul hazelton (<a href="https://codepen.io/hazeltron">@hazeltron</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+
     </inner-column>
 </section>
+
+<section>
+    <inner-column>
+<p class="codepen" data-height="835.01171875" data-default-tab="result" data-slug-hash="VwgraBY" data-editable="true" data-user="hazeltron" style="height: 835.01171875px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/hazeltron/pen/VwgraBY">
+  madlib</a> by paul hazelton (<a href="https://codepen.io/hazeltron">@hazeltron</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+    </inner-column>
+</section>
+
+
+<section>
+	<inner-column>
+	<p class="codepen" data-height="780.7421875" data-slug-hash="YzBaWzM" data-editable="true" data-user="hazeltron" style="height: 780.7421875px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/hazeltron/pen/YzBaWzM">
+  retirement age</a> by paul hazelton (<a href="https://codepen.io/hazeltron">@hazeltron</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+	</inner-column>
+</section>
+
+
+
 
 
 
