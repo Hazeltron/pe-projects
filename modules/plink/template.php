2a074db0a23ff8a172946df5d086816cc88e5735
Hazeltron <paulhazelton.work@gmail.com>
2024-03-04 15:00:54 -0700

    Initialized plink layout

---
+++ modules/plink/template.php
@@ -0,0 +1,29 @@
+<plink-container>
+    <picture>
+        <img src="assets/plink.jpeg" alt="">
+    </picture>
+
+    <text-content>
+        <h2>
+            <span class="loud-voice">enjoy</span>
+            <span class="loud-voice">water</span>
+        </h2>
+
+        <h3 class="lay-attention-voice">Drop Plink! into water for a fruity, fizzy drink.</h3>
+
+        <ul class="lay-attention-voice">
+            <li>Award-winning drink tablets.</li>
+            <li>Loaded with Potassium and Sodium electrolytes</li>
+            <li>Just 1g of sugar per serving</li>
+            <li>A joy for the whole family</li>
+            <li>Without the waste of cans and bottles.</li>
+
+            <a href="#" class="lay-attention-voice">
+                <span>shop</span>
+                <span>all</span>
+                <span>flavors</span>
+                <span>-></span>
+            </a>
+        </ul>
+    </text-content>
+</plink-container>
