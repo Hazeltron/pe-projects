1d60307ea2b01cd860767c0e6db9a8d0681c763f
Hazeltron <paulhazelton.work@gmail.com>
2024-03-05 01:26:01 -0700

    Added necessary classes and removed data types

--- modules/plink/template.php
+++ modules/plink/template.php
@@ -1,29 +1,42 @@
 <plink-container>
-    <picture>
+    <picture class="main-img">
         <img src="assets/plink.jpeg" alt="">
     </picture>
 
     <text-content>
         <h2>
-            <span data-animate="movable">enjoy</span>
-            <span data-animate="movable">water</span>
+            <span>enjoy</span>
+            <span>water</span>
+
+            <picture class="droplet-one">
+                <img src="assets/plink-droplet-one.avif" alt="">
+            </picture>
         </h2>
 
-        <h3 class="lay-attention-voice" data-animate="movable">Drop Plink! into water for a fruity, fizzy drink.</h3>
+        <h3 class="lay-attention-voice">Drop Plink! into water for a fruity, fizzy drink.</h3>
 
-        <ul class="lay-attention-voice" data-animate="movable">
+        <ul class="lay-attention-voice">
             <li>Award-winning drink tablets.</li>
             <li>Loaded with Potassium and Sodium electrolytes</li>
             <li>Just 1g of sugar per serving</li>
             <li>A joy for the whole family</li>
             <li>Without the waste of cans and bottles.</li>
+            <li class="droplet-two">
+                <picture>
+                    <img src="assets/plink-droplet-two.avif" alt="">
+                </picture>
+            </li>
         </ul>
 
         <a href="#" class="lay-attention-voice" >
-            <span data-animate="movable">shop</span>
-            <span data-animate="movable">all</span>
-            <span data-animate="movable">flavors</span>
-            <span data-animate="movable">-></span>
+            <span>shop</span>
+            <span>all</span>
+            <span>flavors</span>
+            <span>
+                <svg class="icon-right-arrow-complete">
+                    <use xlink:href="#icon-right-arrow-complete"></use>
+                </svg>
+            </span>
         </a>
     </text-content>
 </plink-container>
