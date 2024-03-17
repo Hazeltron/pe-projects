86b9d500ace3ade40c9a69ba79794ea5d4e56319
Hazeltron <paulhazelton.work@gmail.com>
2024-03-16 21:45:34 -0600

    Added scroll trigger

--- modules/stickers/controls.js
+++ modules/stickers/controls.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Draggable);
 
 const container = document.querySelector("sticker-container");
@@ -11,6 +12,22 @@ const consoleSticker = document.querySelector("sticker-container .console");
 const flowerLadySticker = document.querySelector("sticker-container .flower-lady");
 
 const sumoSticker = document.querySelector("sticker-container .sumo");
+
+const stickyNote = document.querySelector("sticker-container sticky-note");
+
+// gsap.from("sticker-container h2", {
+//     scrollTrigger: {
+//         trigger: "sticker-container",
+//         toggleActions: "play pause resume reset",
+//         start: "30% 85%",
+//         // markers: true   
+//     },
+//     duration: .2, 
+//     y: 25,
+//     opacity: 0
+// });
+
+
 
 Draggable.create(tigerSticker, {
 	bounds: container, 
@@ -49,6 +66,15 @@ Draggable.create(flowerLadySticker, {
 });
 
 Draggable.create(sumoSticker, {
+	bounds: container, 
+	type: "x,y",
+	onDrag: function() {
+		console.log("dragging !");
+		
+	}
+});
+
+Draggable.create(stickyNote, {
 	bounds: container, 
 	type: "x,y",
 	onDrag: function() {
